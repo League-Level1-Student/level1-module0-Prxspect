3c1365955a80777c86cfaa06d725b8233ce23fca
league <league@wts10.attlocal.net>
2018-05-26 12:57:21 -0700

    MyFirstSwingGUI,FortuneCookie,JackInTheBox,SoundEffects

--- src/FortuneCookie.java
+++ src/FortuneCookie.java
@@ -5,27 +5,41 @@ import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class FortuneCookie implements ActionListener {
 	JFrame frame;
+	JPanel panel;
 	JButton button;
 	public static void main(String[] args) {
 		 FortuneCookie fc = new FortuneCookie();
 	     fc.showButton();
 	}
 	public void showButton() {
-	    frame.setVisible(true);
-	    frame.add(button);
+		JFrame frame=new JFrame();
+	  frame.setVisible(true);
+	  JPanel panel=new JPanel();
+	  frame.add(panel);
+	 	  JButton button=new JButton();
+	   panel.add(button);
+	   frame.pack();
 	    button.addActionListener(this);
-	    int rand = new Random().nextInt(5);
+	   
 	    
 
 
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		int rand= new Random().nextInt(5);
 		// TODO Auto-generated method stub
-		JOptionPane.showMessageDialog(null, "WooHoo");
+		if (rand==0) {
+	    	JOptionPane.showMessageDialog(null, "You will be a millionaire");
+	    }
+		else {
+			JOptionPane.showMessageDialog(null, "No fortune");
+		}
+		
 	}
 	
 }
