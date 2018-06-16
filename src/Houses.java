a2221c0b4f2130b2ffb8f66942fe20ab226907e7
league <league@imac-8.attlocal.net>
2018-06-16 12:58:42 -0700

    Houses

--- src/Houses.java
+++ src/Houses.java
@@ -7,22 +7,51 @@ public static void main(String[] args) {
 	it.setX(10);
 	it.setY(350);
 	drawFlatRoof(100);
+	drawPointyRoof(70);
+	drawFlatRoof(20);
+	drawPointyRoof(90);
+	drawFlatRoof(50);
+	drawPointyRoof(30);
+	drawFlatRoof(10);
+	drawPointyRoof(60);
+	drawFlatRoof(110);
+	drawPointyRoof(40);
+	drawFlatRoof(100);
+	drawPointyRoof(50);
+	
 }
-public void drawPointyRoof() {
+public static void drawPointyRoof(int height) {
+	it.setSpeed(200);
+	it.penDown();
+	it.setPenWidth(3);
+	it.setPenColor(0, 0, 0);
+	it.move(height);
+	it.turn(45);
+	it.move(25);
+	it.turn(90);
+	it.move(25);
+	it.turn(45);
+	it.move(height);
+	it.turn(-90);
+	it.setPenColor(0, 255, 0);
+	it.move(40);
+	it.turn(-90);
 	
 }
 public static void drawFlatRoof(int height) {
 	
-	it.setSpeed(50);
+	it.setSpeed(200);
 	it.penDown();
 	it.setPenWidth(3);
+	it.setPenColor(0, 0, 0);
 	it.move(height);
 	it.turn(90);
-	it.move(50);
+	it.move(35);
 	it.turn(90);
 	it.move(height);
 	it.turn(-90);
 	it.setPenColor(0, 255, 0);
-	it.move(50);
+	it.move(40);
+	it.turn(-90);
 }
 }
